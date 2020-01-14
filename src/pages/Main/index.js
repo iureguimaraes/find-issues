09210f7e56d21284c230b43e67d5d169d62b19d7
Iure Guimarães <iurevguimaraes@gmail.com>
2020-01-14 18:40:30 -0300

    repositories style

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -37,7 +37,7 @@ export default class Main extends Component {
   };
 
   render() {
-    const { newRepo, loading } = this.state;
+    const { newRepo, repositories, loading } = this.state;
 
     return (
       <Container>
@@ -62,6 +62,15 @@ export default class Main extends Component {
             )}
           </SubmitButton>
         </Form>
+
+        <List>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <a href="#">Detalhes</a>
+            </li>
+          ))}
+        </List>
       </Container>
     );
   }
